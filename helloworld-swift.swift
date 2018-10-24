36bceb1cba4523aeb5103de640d18b77d1260516
ClintJang <jang.wangsu@gmail.com>
2018-10-24 21:37:35 +0900

    Add : Add a file

---
+++ helloworld-swift.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct IOSSwiftObjcQuestionsAndAnswers {
+    static func hello() -> Void {
+        print("iOS(Swift, Objc) 질문과 답변 모음에 방문해 주셔서 환영합니다. ")
+    }
+    
+    static func info() -> Void {
+        print("iOS (swift, objc)개발을 하면서 알게된 질문과 답변 내용을 모아보았습니다. 개발 언어는 공부를 하면 빠르게 익힐 수 있지만, 개발 경험은 물어보고 정보를 얻기가 쉽지 않습니다. 도움이 될 수 있도록 정리를 꼼꼼히 해보겠습니다.")
+    }
+}
